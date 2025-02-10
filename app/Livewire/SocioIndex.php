8ba3731261dbf6c16183077995b5780192b5005f
Jaime García <jaime.garcia@iesdonana.org>
2025-02-10 15:58:24 +0100

    CRUD de socios con livewire

--- app/Livewire/SocioIndex.php
+++ app/Livewire/SocioIndex.php
@@ -2,12 +2,56 @@
 
 namespace App\Livewire;
 
+use App\Models\Socio;
 use Livewire\Component;
 
 class SocioIndex extends Component
 {
+    public $codigo;
+    public $nombre;
+    public $socioid;
+    public $estaEditando = false;
+
+    public function crear()
+    {
+        Socio::create($this->pull());
+    }
+
+    public function editar($socioid)
+    {
+        $this->socioid = $socioid;
+        $socio = Socio::findOrFail($socioid);
+        $this->codigo = $socio->codigo;
+        $this->nombre = $socio->nombre;
+        $this->estaEditando = true;
+    }
+
+    public function actualizar()
+    {
+        // Hace la actualización
+        $socio = Socio::findOrFail($this->socioid);
+        $socio->fill(
+            $this->pull()
+        );
+        $socio->save();
+        $this->reset();
+    }
+
+    public function eliminar($socioid)
+    {
+        $socio = Socio::findOrFail($socioid);
+        $socio->delete();
+    }
+
+    public function cancelar()
+    {
+        $this->reset();
+    }
+
     public function render()
     {
-        return view('livewire.socio-index');
+        return view('livewire.socio-index')->with([
+            'socios' => Socio::all(),
+        ]);
     }
 }
--- resources/views/livewire/socio-index.blade.php
+++ resources/views/livewire/socio-index.blade.php
@@ -21,3 +21,44 @@
         <button wire:click.prevent="cancelar" @click="open = ! open"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancelar</button>
     </form>
+
+    <div class="relative overflow-x-auto mt-5">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Código
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nombre
+                    </th>
+                    <th scope="col" colspan="2" colspan="2" class="px-6 py-3">
+                        Acciones
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($socios as $socio)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $socio->codigo }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $socio->nombre }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" wire:confirm="¿Seguro?" wire:click="eliminar({{ $socio->id }})"
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Eliminar</a>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" wire:click="editar({{ $socio->id }})" @click="open = ! open"
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</div>
